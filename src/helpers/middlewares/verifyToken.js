1ab5878dd923e59c656a2b7118bbf032b3ef65f9
woobwoofwolf <imbakakak@gmail.com>
2020-12-07 20:13:52 +0700

    simple logout scheme using token blocklisting

--- src/helpers/middlewares/verifyToken.js
+++ src/helpers/middlewares/verifyToken.js
@@ -1,28 +1,59 @@
-const jwt = require("jsonwebtoken");
+const jsonwebtoken = require('jsonwebtoken')
+const db = require('../../config/mySQL')
 
-const form = require("../form");
 
 module.exports = (req, res, next) => {
-  const bearerToken = req.header("x-access-token");
-  if (!bearerToken) {
-    form.error(res, {
-      msg: "Please Login First",
-      status: 401,
-    });
-  } else {
+    const bearerToken = req.header("x-access-token");
+    //jika tidak ada header x-access-token
+    if (!bearerToken) {
+        res.json({
+            msg: `Please login first`,
+            status: 401 //unauthorized access
+        })
+    } else {
     // Bearer Token
     // [Bearer, Token]
-    const token = bearerToken.split(" ")[1];
-    try {
-      const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-      req.decodedToken = decodedToken;
-      next();
-    } catch (error) {
-      form.error(res, {
-        msg: "Invalid Token",
-        error,
-        status: 401,
-      });
-    }
+    const token = bearerToken.split(" ")[1]
+    console.log(token)
+    const checkBlacklist = new Promise((resolve, reject) => {
+      const queryStr = `SELECT token FROM blocklist_token WHERE token = ?`
+      console.log(queryStr)
+      db.query(queryStr, token, (err, data) => {
+        if (!err) {
+          if (!data[0]) {
+            console.log('a')
+            resolve(token)
+          } else {
+            console.log('b')
+            reject({
+              msg: `Invalid token => ${data[0].token}, cannot user anymore. Already logout`
+            })
+          }
+        } else {
+          console.log('c')
+          reject({
+            msg: `check Token ERROR!`
+          })
+        }
+      })
+    }).then((result) => {
+      //cek decodedToken apakah cocok
+      try {
+        console.log(result)
+        decodedToken = jsonwebtoken.verify(result, process.env.SECRET_KEY)
+        //asign decodedToken to req
+        req = {
+          ...req,
+          decodedToken
+        }
+        next() //meneruskan ke proses selanjutnya
+      } catch (err) {
+        res.json({
+          msg: `Token invalid, wrong SECRET_KEY`
+        })
+      }
+    }).catch((error) => {
+      res.json(error)
+    })
   }
-};
+}
