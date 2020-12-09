ce06804a005877bd9fd88f28dd8aa39fe7ac25e2
woobwoofwolf <imbakakak@gmail.com>
2020-12-09 20:46:55 +0700

    fixing some routes

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,22 +2,26 @@ const express = require('express')
 const productRouter = express.Router()
 const productController = require('../controllers/product')
 const multiUpload = require("../helpers/middlewares/upload");
+const verifyToken = require("../helpers/middlewares/verifyToken");
 
 //get by id
 productRouter.get('/:id', productController.getById )
 
 //add New Product
-productRouter.post("/add-product", multiUpload,productController.addNew)
+productRouter.post("/add-product", verifyToken.isSeller, multiUpload, productController.addNew)
 
 //add from Existing Product
-productRouter.post("/add-stock", productController.addExisting) 
+productRouter.post("/add-stock", verifyToken.isSeller, productController.addExisting) 
 
 
-//update pivot
-productRouter.patch("/update", productController.updateProduct)
+//update pivot table using pivotid
+productRouter.patch("/update", verifyToken.isSeller, productController.updateProduct)
 
+//DELETE
+productRouter.delete("/delete/:id", verifyToken.isSeller, productController.deleteProduct)
+
+//getsize & color for front-end
 productRouter.get("/get_size/:id", productController.getSize)
-
 productRouter.get("/get_color/:id", productController.getColor)
 
 module.exports = productRouter
