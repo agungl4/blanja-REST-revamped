c1b8c3080c5c235e274ff41783a02a4454fdda52
woobwoofwolf <imbakakak@gmail.com>
2020-12-09 20:50:55 +0700

    fixing main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,13 +9,12 @@ const transactionRouter = require("./transaction")
 const authRouter = require('./auth')
 
 const verifyToken = require("../helpers/middlewares/verifyToken");
-// const imgUploadRouter = require("./imgUpload");
 
 
 //calling endpoint handler
 mainRouter.use("/", welcomeRouter)
-mainRouter.use("/products", productsRouter) // endpoint sort, delete
-mainRouter.use("/product", productRouter) // endpoint insert, update
+mainRouter.use("/products", productsRouter) // endpoint sort, 
+mainRouter.use("/product", verifyToken.isLogin, productRouter) // endpoint insert, update , delete
 mainRouter.use("/search", searchRouter) // endpoint filter
 mainRouter.use("/transaction", transactionRouter)
 mainRouter.use("/auth", authRouter)
