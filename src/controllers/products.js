7ce83e6d2665964308bf647fbef7c7c8dd527607
woobwoofwolf <imbakakak@gmail.com>
2020-12-10 04:38:14 +0700

    pagination

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -4,20 +4,34 @@ const form = require('../helpers/form')
 module.exports = {
   sortProduct: (req, res) => {
     const { sortBy, orderBy } = req.query;
-
+    const { query } = req;
     let addQuery = ``
-
+    const limit = Number(query.limit) || 2;
+    const page = Number(query.page) || 1;
+    const offset = (page - 1) * limit || 0;
+    let urlQuery =``
     if (sortBy != null) {
       if (orderBy == `desc`) {
         addQuery += `ORDER BY  ${sortBy} DESC`
-      }else{
+        urlQuery = `sortBy=${sortBy}&orderBy=desc&`
+      } else {
         addQuery += `ORDER BY ${sortBy} ASC`
+        urlQuery = `sortBy=${sortBy}&orderBy=asc&`
       }
+    } else {
+      addQuery += `ORDER BY product_id ASC`
     }
 
-    sortModel.sortProduct(addQuery)
+    sortModel.sortProduct(addQuery,urlQuery, limit, offset, page)
       .then((data) => {
-        form.success(res, data)
+        if (Math.ceil(data.products / limit) == data.products) {
+          res.status(404).json({
+            msg: "Page Not Found",
+            status: 404,
+          });
+        } else {
+          form.success(res, data)
+        }
       })
       .catch((err) => {
         form.error(res, err)
--- src/models/products.js
+++ src/models/products.js
@@ -1,17 +1,26 @@
 const db = require('../config/mySQL')
 module.exports = {
-  sortProduct: (param) => {
+  sortProduct: (param, param2, limit, offset, page) => {
     return new Promise((resolve, reject) => {
       let queryString = `SELECT m.id, m.product_id, p.product_name, c.category_name, pc.color_name, ps.size_name, pco.condition_name, p.product_price, p.product_desc, p.product_img, m.qty, m.created_at, m.updated_at 
             FROM master m JOIN products p ON m.product_id = p.id 
             JOIN category c ON p.category_id = c.id 
             JOIN color pc ON m.color_id = pc.id 
             JOIN size ps ON m.size_id = ps.id 
-            JOIN conditions pco ON m.condition_id = pco.id `
-      queryString += param
-      db.query(queryString, (err, data) => {
+            JOIN conditions pco ON m.condition_id = pco.id ` +param+ ` LIMIT ? OFFSET ?`
+      db.query(queryString, [limit, offset], (err, data) => {
+        console.log(queryString)
+        const newResult = {
+          products: data,
+          pageInfo: {
+            currentPage: page,
+            previousPage:
+              page === 1 ? null : `/products?${param2}page=${page - 1}&limit=${limit}`,
+            nextPage: limit != data.length ? null : `/products?${param2}page=${page + 1}&limit=${limit}`,
+          },
+        };
         if (!err) {
-          resolve(data)
+          resolve(newResult)
         } else {
           reject({
             msg: `Please input sorting params first`
